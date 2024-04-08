958dd7a0dcebaaa403361cab8ae7eb19ef7d61fd
Liviter <lovehan27793342@gmail.com>
2024-04-09 00:11:07 +0800

    Update update_version.sh

--- clashconf/update_version.sh
+++ clashconf/update_version.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # 读取版本号
-version=$(cat version.txt)
+version_file="version.txt"
+version=$(cat "$version_file")
 echo "当前版本号为：$version"
 # 将版本号从字符串转换为浮点数，并增加0.1
 new_version=$(echo "$version + 0.1" | bc)
 
 # 将新版本号写入 version.txt
-echo "$new_version" > version.txt
+echo "$new_version" > $version_file
 
 echo "版本号已更新为： $new_version"
