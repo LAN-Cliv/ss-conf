00cf9293330d3491a67ee6f2b7d54d3052aa5007
Liviter <lovehan27793342@gmail.com>
2024-04-08 23:46:07 +0800

    Update subconf.sh

--- clashconf/subconf.sh
+++ clashconf/subconf.sh
@@ -7,16 +7,29 @@ wget -p https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/AC
 ruleset_file="ruleset.list"
 proxy_group_file="proxy_group.list"
 
-# 检查文件是否存在
-for file in "$ruleset_file" "$proxy_group_file"; do
-    if [ ! -f "$file" ]; then
-        echo "文件 $file 不存在"
-        exit 1
-    fi
-done
+# 如果版本号文件不存在，创建并设置版本号为1.0
+version_file="version.txt"
+if [ ! -f "$version_file" ]; then
+    echo "1.0" > "$version_file"
+fi
+
+# 读取当前版本号
+current_version=$(cat "$version_file")
+
+# 更新版本号
+next_version=$(awk '{print $1 + 0.1}' <<< "$current_version")
+
+# 添加版本号注释
+sed -i "1i #当前版本号为：${next_version}" clashconf.ini
+
+# 更新版本号文件
+echo "$next_version" > "$version_file"
+
+echo "配置已更新，当前版本号为：${next_version}"
+
+
 
 # 修改配置文件
-
 # 在插入内容
 sed -i '0,/^;设置规则标志位/ { /^;设置规则标志位/ r '"$ruleset_file"'
 }' clashconf.ini
