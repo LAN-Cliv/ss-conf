29a204c1175655e2947a194f8b57b98aaf8e2be6
Liviter <lovehan27793342@gmail.com>
2024-04-09 00:41:18 +0800

    Update script.sh

--- script/script.sh
+++ script/script.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
 
 # 下载文件
-wget https://johnshall.github.io/Shadowrocket-ADBlock-Rules-Forever/lazy_group.conf -O lazy_group.conf
+wget https://johnshall.github.io/Shadowrocket-ADBlock-Rules-Forever/lazy_group.conf -O diy.conf
 
 # 提取信息
 rule_file="rule.list"
 proxy_group_file="proxy_group.list"
 host_file="host.list"
 
-# 检查文件是否存在
-for file in "$rule_file" "$proxy_group_file" "$host_file"; do
-    if [ ! -f "$file" ]; then
-        echo "文件 $file 不存在"
-        exit 1
-    fi
-done
+# 获取版本号文件
+version_file="version.txt"
+
+# 读取当前版本号
+current_version=$(cat "$version_file")
+
+# 添加版本号注释
+sed -i "1i #当前版本号为：${current_version}" diy.conf
+echo "配置已更新，当前版本号为：${current_version}"
 
 # 修改配置文件
 
 # 删除局域网相关的192.168.0.0/16
-sed -i 's/192\.168\.0\.0\/16,//' lazy_group.conf
+sed -i 's/192\.168\.0\.0\/16,//' diy.conf
 
 # 在 [Rule] 下插入内容
-sed -i "/\[Rule\]/r $rule_file" lazy_group.conf
+sed -i "/\[Rule\]/r $rule_file" diy.conf
 
 # 在 [Proxy Group] 下插入内容
-sed -i "/\[Proxy Group\]/r $proxy_group_file" lazy_group.conf
+sed -i "/\[Proxy Group\]/r $proxy_group_file" diy.conf
 
 # 在 [Host] 下插入内容
-sed -i "/\[Host\]/r $host_file" lazy_group.conf
-
+sed -i "/\[Host\]/r $host_file" diy.conf
